5ee95042d45ac60712cddc03b865ec786faf17d9
GVS <godavarti@live.in>
2023-10-15 13:40:47 -0700

    Added Proof for Singleton DP objects

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,14 @@ Console.WriteLine("Hello, World!");
 //Singleton design patterns demonstration 
 Loggersingleton log= Loggersingleton.GetLoggersingleton(); 
 log.printmessage(" I am logging for you GVS");
+//How to prove, that singleton will create only one object , GetHashCode() method for log1 and log2 reference returns the same value
+
+Loggersingleton log1= Loggersingleton.GetLoggersingleton(); 
+log.printmessage(" I am logging for you GVS_"+ log1.GetHashCode().ToString());
+Loggersingleton log2= Loggersingleton.GetLoggersingleton(); 
+log.printmessage(" I am logging for you GVS_"+ log1.GetHashCode().ToString());
+
 LoggerInstance logI = LoggerInstance.Instance;
- logI.printmessage(" I am fromm LoggerIsntance ");
+ logI.printmessage(" I am fromm LoggerIsntance");
  }
 }
